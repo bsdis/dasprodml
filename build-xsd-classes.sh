6de180dbe1a3ac9bf1da4d8f2495e780e5aad2b1
Eugene Petkevich <nasedil.genio.code@gmail.com>
2017-04-29 13:45:54 +0300

    Fix generateds script

--- build-xsd-classes.sh
+++ build-xsd-classes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script creates python modules with classes representing prodml schemas.
-generateDS.py -o dasprodml/DasAcquisition.py -s dasprodml/DasAcquisitionSub.py --super=DasAcquisition -m ./energistics/prodml/v2.0/xsd_schemas/DasAcquisition.xsd
-generateDS.py -o dasprodml/FiberOpticalPath.py -s dasprodml/FiberOpticalPathSub.py --super=DasAcquisition -m ./energistics/prodml/v2.0/xsd_schemas/FiberOpticalPath.xsd
+generateDS.py -o dasprodml/DasAcquisition.py -s dasprodml/DasAcquisitionSub.py --super=dasprodml.DasAcquisition -m ./energistics/prodml/v2.0/xsd_schemas/DasAcquisition.xsd
+generateDS.py -o dasprodml/FiberOpticalPath.py -s dasprodml/FiberOpticalPathSub.py --super=dasprodml.FiberOpticalPath -m ./energistics/prodml/v2.0/xsd_schemas/FiberOpticalPath.xsd
